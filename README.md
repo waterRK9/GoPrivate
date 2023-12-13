ba2bf83e32cdbe324c187c6934cd46dc55116d10
Aileen Han <ahan@mit.edu>
2023-12-13 15:33:46 -0500

    Wrote ReadMe and also cleaned up encrypted_ops_test file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # GoPrivate
 # GoPrivate
+
+To run queries in GoPrivate, much of the same process as godb is used. The difference between these
+two database systems occurs when running encrypted aggregation queries. To run an encrypted aggregation
+query in GoPrivate, the following steps should be followed:
+
+1. Define the query text and the tuple descriptor for your data. 
+2. Define the file path to your (unencrypted) data, as well as the file path to where you would like 
+   your encrypted data to be stored. If you would like to use multiple tables in your query, define
+   these two file paths for each table. 
+3. Pass in the tuple descriptor, input file path, output file path, and the query text for the first
+   table to CSVToEncryptedDat to get the encrypted heap file for that table, as well as the encryption
+   scheme.
+4. If using multiple tables, for each subsequent table, pass in the tuple descriptor, input file path, 
+   output file path, and the encryption scheme that was output by CSVToEncryptedDat to CSVToEncryptedDatGivenE 
+   to get the encrypted heap file for that subsequent table. 
+5. Now that each table has been encrypted and put into a heap file, you are ready to run your aggregation
+   operation. First, if using multiple tables, join them together using a vertical-join: a vertical join 
+   is initialized using the NewVerticalJoin function, called with a list of the encrypted heap files and
+   the max buffer size. The default for the max buffer size is 100. 
+6. Next, to do the aggregation, and encrypted aggregation state should be created. Currently, GoPrivate
+   supports 3 aggregation operations: average, for which the corresponding aggregation state is 
+   EncryptedAvgAggState; sum, for which the corresponding aggregation state is EncryptedSumAggState, and
+   count, for which the corresponding aggregation state is EncryptedCountAggState. 
+7. Then, an expression denoting which field should be aggregated over should be created. This is exactly 
+   the same as in godb.
+8. The encrypted aggregation state should be initialized using this expression. An additional parameter
+   for initializing encrypted aggregation states is the public encryption key for the aggregation column, 
+   which can be retrieved from the PublicKeys array in the encryption scheme.
+9. Finally, the encrypted aggregator can be created using NewEncryptedAggregator on the encrypted aggregation
+   state and the encrypted heap file (or the result of the vertical join if using multiple tables). This 
+   encrypted aggregator can be used in much of the same way as the unencrypted aggregators in the original
+   godb, although the output tuple will be encrypted, and must be decrypted to retrieve the unencrypted
+   aggregation result. 
+
+Examples of this process can be found in encrypted_ops_test.go, which tests simple queries for each type of 
+aggregation (average, count, and sum), and for count and average (since sum is very similar to average), tests 
+queries with and without vertical joins, with and without filtering, and for count, with and without the distinct
+keyword. 
--- go-private-db/godb/notes.go
+++ go-private-db/godb/notes.go
@@ -1,14 +1,62 @@
 package godb
 
-import (
-	"fmt"
-	"os"
-	"testing"
-	"time"
-)
+// func TestEncryptedAvgAgg(t *testing.T) {
+// 	sql := "select avg(age) from t"
+// 	bp := NewBufferPool(10)
+// 	hf, err := MakeTestPatientDatabase(bp)
+// 	if err != nil {
+// 		t.Errorf("%s", err.Error())
+// 		return
+// 	}
 
-// / Encryped AVG Test ///
-func TestEncryptedAvgAgg(t *testing.T) {
+// 	err, e := translateQuery(sql)
+// 	if err != nil {
+// 		t.Errorf("%s", err.Error())
+// 		return
+// 	}
+
+// 	tid := NewTID()
+// 	bp.BeginTransaction(tid)
+// 	encryptedHf, err := e.encryptOrDecrypt(hf, "encrypted_patients.dat", true, tid)
+// 	if err != nil {
+// 		t.Errorf("%s", err.Error())
+// 		return
+// 	}
+
+// 	aa := EncryptedAvgAggState[string]{}
+// 	expr := FieldExpr{FieldType{Fname: "age", TableQualifier: "t"}}
+// 	aa.Init("avg", &expr, stringAggGetter, *e.PublicKeys["age"])
+// 	agg := NewEncryptedAggregator([]EncryptedAggState{&aa}, encryptedHf)
+
+// 	iter, err := agg.Iterator(tid)
+// 	if err != nil {
+// 		t.Fatalf(err.Error())
+// 	}
+// 	if iter == nil {
+// 		t.Fatalf("Iterator was nil")
+// 	}
+// 	tup, err := iter()
+// 	if err != nil {
+// 		t.Fatalf(err.Error())
+// 	}
+// 	if tup == nil {
+// 		t.Fatalf("Expected non-null tuple")
+// 	}
+
+// 	result, err := e.encryptOrDecryptTuple(tup, false)
+// 	if err != nil {
+// 		t.Fatalf(err.Error())
+// 	}
+
+// 	sum := result.Fields[0].(IntField).Value
+// 	count := result.Fields[1].(IntField).Value
+// 	if sum != 395 || count != 10 {
+// 		t.Errorf("unexpected sum or count")
+// 	}
+// }
+
+// / Encryped AVG Tests ///
+/*func TestEncryptedAvgAgg(t *testing.T) {
 	sql := "select avg(age) from t"
 
 	var td = TupleDesc{Fields: []FieldType{
@@ -63,7 +111,7 @@ func TestEncryptedAvgAgg(t *testing.T) {
 	}
 }
 
-// / Unencrypted AVG Test///
+// / Unencrypted AVG Tests///
 func TestAvgAgg(t *testing.T) {
 	var td = TupleDesc{Fields: []FieldType{
 		{Fname: "id", Ftype: StringType},
@@ -119,9 +167,59 @@ func TestAvgAgg(t *testing.T) {
 	fmt.Println(tup)
 }
 
-// / Encryped COUNT Test ///
+/*func TestEncryptedCountAgg(t *testing.T) {
+	sql := "select count(ssn) from t"
 
-func TestEncryptedCountAgg(t *testing.T) {
+	var td = TupleDesc{Fields: []FieldType{
+		{Fname: "id", Ftype: StringType},
+		{Fname: "ssn", Ftype: StringType},
+		{Fname: "first_name", Ftype: StringType},
+		{Fname: "last_name", Ftype: StringType},
+		{Fname: "phone_number", Ftype: StringType},
+		{Fname: "gender", Ftype: StringType},
+		{Fname: "age", Ftype: StringType},
+		{Fname: "diagnosis_code", Ftype: StringType},
+	}}
+
+	inputFileName := "encryptedresults/298_mock_patient_data.csv"
+	resultFileName := "encryptedresults/298_encrypted_mock_patient_data.dat"
+
+	encryptedHf, e := CSVToEncryptedDat(td, inputFileName, resultFileName, sql)
+
+	aa := EncryptedCountAggState{}
+	expr := FieldExpr{FieldType{Fname: "ssn", TableQualifier: "t", Ftype: StringType}}
+	aa.Init("count", &expr, stringAggGetter, *e.PublicKeys["ssn"])
+	agg := NewEncryptedAggregator([]EncryptedAggState{&aa}, encryptedHf)
+
+	iter, err := agg.Iterator(nil)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if iter == nil {
+		t.Fatalf("Iterator was nil")
+	}
+	tup, err := iter()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if tup == nil {
+		t.Fatalf("Expected non-null tuple")
+	}
+
+	result, err := e.encryptOrDecryptTuple(tup, false)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	print("yes")
+	//sum := result.Fields[0].(IntField).Value
+	count := result.Fields[0].(IntField).Value
+	if count != 298 {
+		t.Errorf("unexpected count")
+	}
+}*/
+
+/*func TestEncryptedCountAgg(t *testing.T) {
 	sql := "select count(ssn) from t"
 
 	var td = TupleDesc{Fields: []FieldType{
@@ -178,10 +276,8 @@ func TestEncryptedCountAgg(t *testing.T) {
 	elapsed := time_now.Sub(start)
 	fmt.Println(elapsed)
 
-	//t.Fatalf("Expected non-null tuple")
+	t.Fatalf("Expected non-null tuple")
 }
-
-// / Unencryped COUNT Test ///
 
 func TestCountAgg(t *testing.T) {
 	var td = TupleDesc{Fields: []FieldType{
@@ -240,128 +336,12 @@ func TestCountAgg(t *testing.T) {
 	elapsed := time_now.Sub(start)
 	fmt.Println(elapsed)
 
-	//t.Fatalf("Expected non-null tuple")
+	t.Fatalf("Expected non-null tuple")
 
 }
 
-// / Encryped SUM Test ///
-func TestEncryptedSumAgg(t *testing.T) {
-	sql := "select sum(age) from t"
-
-	var td = TupleDesc{Fields: []FieldType{
-		{Fname: "id", Ftype: StringType},
-		{Fname: "ssn", Ftype: StringType},
-		{Fname: "first_name", Ftype: StringType},
-		{Fname: "last_name", Ftype: StringType},
-		{Fname: "phone_number", Ftype: StringType},
-		{Fname: "gender", Ftype: StringType},
-		{Fname: "age", Ftype: StringType},
-		{Fname: "diagnosis_code", Ftype: StringType},
-	}}
-	resultFileName := "encryptedresults/1000_encrypted_mock_patient_data.csv"
-
-	// Uncomment Only Below for First Run to Generate Encrypted File
-	// inputFileName := "encryptedresults/1000_mock_patient_data.csv"
-	// encryptedHf, e := CSVToEncryptedDat(td, inputFileName, resultFileName, sql)
-
-	// Uncomment Only Below for Subsequent Runs to Avoid Generating Encrypted File
-	err, e := translateQuery(sql)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	bp := NewBufferPool(10)
-	encryptedHf, err := NewHeapFile(resultFileName, &td, bp)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	// ///
-
-	aa := EncryptedSumAggState[string]{}
-	expr := FieldExpr{FieldType{Fname: "age", TableQualifier: "t"}}
-	aa.Init("sum", &expr, stringAggGetter, *e.PublicKeys["age"])
-
-	agg := NewEncryptedAggregator([]EncryptedAggState{&aa}, encryptedHf)
-	iter, err := agg.Iterator(nil)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if iter == nil {
-		t.Fatalf("Iterator was nil")
-	}
-	tup, err := iter()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if tup == nil {
-		t.Fatalf("Expected non-null tuple")
-	}
-	fmt.Println(tup)
-}
-
-// / Unencryped SUM Test ///
-func TestSumAgg(t *testing.T) {
-	var td = TupleDesc{Fields: []FieldType{
-		{Fname: "id", Ftype: StringType},
-		{Fname: "ssn", Ftype: StringType},
-		{Fname: "first_name", Ftype: StringType},
-		{Fname: "last_name", Ftype: StringType},
-		{Fname: "phone_number", Ftype: StringType},
-		{Fname: "gender", Ftype: StringType},
-		{Fname: "age", Ftype: IntType},
-		{Fname: "diagnosis_code", Ftype: StringType},
-	}}
-
-	resultFileName := "encryptedresults/1000_unencrypted_mock_patient_data.dat"
-	os.Remove(resultFileName)
-
-	bp := NewBufferPool(10)
-	hf, err := NewHeapFile(resultFileName, &td, bp)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	// Uncomment the below on first run to generate dat file
-	inputFileName := "encryptedresults/1000_mock_patient_data.csv"
-	f, err := os.Open(inputFileName)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	err = hf.LoadFromCSV(f, true, ",", false)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	////
-
-	sa := SumAggState[int64]{}
-	expr := FieldExpr{FieldType{Fname: "age", TableQualifier: "t"}}
-	sa.Init("sum", &expr, intAggGetter)
-	agg := NewAggregator([]AggState{&sa}, hf)
-
-	iter, err := agg.Iterator(nil)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if iter == nil {
-		t.Fatalf("Iterator was nil")
-	}
-	tup, err := iter()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if tup == nil {
-		t.Fatalf("Expected non-null tuple")
-	}
-	fmt.Println(tup)
-}
-
-// / Encryped AVG Test with Filter ///
 func TestEncryptedAvgAggWhere(t *testing.T) {
-	//the query we are trying to execute
 	//"select avg(age) from t where diagnosis_code = "S61519A" "
-
-	//the query translator is quite simplistic at this moment, so we
-	//pass in a simpler query that has the same encryption scheme instead
 	sql := "select avg(age) from t"
 
 	var td = TupleDesc{Fields: []FieldType{
@@ -442,14 +422,8 @@ func TestEncryptedAvgAggWhere(t *testing.T) {
 	}
 }
 
-// / Encryped COUNT Test with Filter ///
-
 func TestEncryptedCountAggWhere(t *testing.T) {
-	//the query we are trying to execute
 	//"select count(ssn) from t where diagnosis_code = "S61519A" "
-
-	//the query translator is quite simplistic at this moment, so we
-	//pass in a simpler query that has the same encryption scheme instead
 	sql := "select count(ssn) from t"
 
 	var td = TupleDesc{Fields: []FieldType{
@@ -529,8 +503,6 @@ func TestEncryptedCountAggWhere(t *testing.T) {
 	}
 }
 
-// / Encryped AVG Test with Vertical Join ///
-
 func TestEncryptedAvgAggVertiJoin(t *testing.T) {
 	sql := "select avg(age) from t"
 
@@ -551,6 +523,7 @@ func TestEncryptedAvgAggVertiJoin(t *testing.T) {
 	resultFileName2 := "encryptedresults/other_small_encrypted_mock_patitent_data.csv"
 
 	encryptedHf1, e := CSVToEncryptedDat(td, inputFileName1, resultFileName1, sql)
+	//encryptedHf2, e := CSVToEncryptedDat(td, inputFileName2, resultFileName2, sql)
 	encryptedHf2 := CSVToEncryptedDatGivenE(td, inputFileName2, resultFileName2, e)
 
 	join, err := NewVerticalJoin([]Operator{encryptedHf1, encryptedHf2}, 100)
@@ -592,76 +565,8 @@ func TestEncryptedAvgAggVertiJoin(t *testing.T) {
 	}
 }
 
-// / Encryped COUNT Test with Vertical Join ///
-
-func TestEncryptedCountAggVertiJoin(t *testing.T) {
-	sql := "select count(ssn) from t"
-
-	var td = TupleDesc{Fields: []FieldType{
-		{Fname: "id", Ftype: StringType},
-		{Fname: "ssn", Ftype: StringType},
-		{Fname: "first_name", Ftype: StringType},
-		{Fname: "last_name", Ftype: StringType},
-		{Fname: "phone_number", Ftype: StringType},
-		{Fname: "gender", Ftype: StringType},
-		{Fname: "age", Ftype: StringType},
-		{Fname: "diagnosis_code", Ftype: StringType},
-	}}
-
-	inputFileName1 := "encryptedresults/small_mock_patitent_data.csv"
-	resultFileName1 := "encryptedresults/small_encrypted_mock_patitent_data.csv"
-	inputFileName2 := "encryptedresults/other_small_mock_patitent_data.csv"
-	resultFileName2 := "encryptedresults/other_small_encrypted_mock_patitent_data.csv"
-
-	encryptedHf1, e := CSVToEncryptedDat(td, inputFileName1, resultFileName1, sql)
-	encryptedHf2 := CSVToEncryptedDatGivenE(td, inputFileName2, resultFileName2, e)
-
-	join, err := NewVerticalJoin([]Operator{encryptedHf1, encryptedHf2}, 100)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	aa := EncryptedCountAggState{}
-	expr := FieldExpr{FieldType{Fname: "ssn", TableQualifier: "t"}}
-	aa.Init("count", &expr, stringAggGetter, *e.PublicKeys["ssn"])
-	agg := NewEncryptedAggregator([]EncryptedAggState{&aa}, join)
-
-	iter, err := agg.Iterator(nil)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if iter == nil {
-		t.Fatalf("Iterator was nil")
-	}
-	tup, err := iter()
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	if tup == nil {
-		t.Fatalf("Expected non-null tuple")
-	}
-
-	result, err := e.encryptOrDecryptTuple(tup, false)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-
-	count := result.Fields[0].(IntField).Value
-
-	fmt.Println(count)
-	if count != 16 {
-		t.Errorf("unexpected sum or count")
-	}
-}
-
-// / Encryped COUNT Test with DISTINCT and Vertical Join ///
-
 func TestEncryptedCountAggVertiJoinDistinct(t *testing.T) {
-	//the query we are trying to execute
-	//"select count(ssn) from (select distinct ssn from t)"
-
-	//the query translator is quite simplistic at this moment, so we
-	//pass in a simpler query that has the same encryption scheme instead
+	//sql := "select count(ssn) from (select distinct ssn from t)"
 	sql := "select count(ssn) from t"
 
 	var td = TupleDesc{Fields: []FieldType{
@@ -681,6 +586,7 @@ func TestEncryptedCountAggVertiJoinDistinct(t *testing.T) {
 	resultFileName2 := "encryptedresults/other_small_encrypted_mock_patitent_data.csv"
 
 	encryptedHf1, e := CSVToEncryptedDat(td, inputFileName1, resultFileName1, sql)
+	//encryptedHf2, e := CSVToEncryptedDat(td, inputFileName2, resultFileName2, sql)
 	encryptedHf2 := CSVToEncryptedDatGivenE(td, inputFileName2, resultFileName2, e)
 
 	join, err := NewVerticalJoin([]Operator{encryptedHf1, encryptedHf2}, 100)
@@ -728,3 +634,206 @@ func TestEncryptedCountAggVertiJoinDistinct(t *testing.T) {
 		t.Errorf("unexpected sum or count")
 	}
 }
+
+func TestEncryptedAvgAggVertiJoinDistinct(t *testing.T) {
+	//sql := "select avg(age) from (select ssn, avg(age) from t group by ssn)"
+	sql := "select avg(age) from t"
+
+	var td = TupleDesc{Fields: []FieldType{
+		{Fname: "id", Ftype: StringType},
+		{Fname: "ssn", Ftype: StringType},
+		{Fname: "first_name", Ftype: StringType},
+		{Fname: "last_name", Ftype: StringType},
+		{Fname: "phone_number", Ftype: StringType},
+		{Fname: "gender", Ftype: StringType},
+		{Fname: "age", Ftype: IntType},
+		{Fname: "diagnosis_code", Ftype: StringType},
+	}}
+
+	inputFileName1 := "encryptedresults/small_mock_patitent_data.csv"
+	resultFileName1 := "encryptedresults/small_encrypted_mock_patitent_data.csv"
+	inputFileName2 := "encryptedresults/other_small_mock_patitent_data.csv"
+	resultFileName2 := "encryptedresults/other_small_encrypted_mock_patitent_data.csv"
+
+	encryptedHf1, e := CSVToEncryptedDat(td, inputFileName1, resultFileName1, sql)
+	//encryptedHf2, e := CSVToEncryptedDat(td, inputFileName2, resultFileName2, sql)
+	encryptedHf2 := CSVToEncryptedDatGivenE(td, inputFileName2, resultFileName2, e)
+
+	join, err := NewVerticalJoin([]Operator{encryptedHf1, encryptedHf2}, 100)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	/*iter_test, _ := join.Iterator(nil)
+	for {
+		t, _ := iter_test()
+		fmt.Println(t)
+		if t == nil {
+			break
+		}
+	}*/
+
+/*var outNames []string = make([]string, 2)
+outNames[0] = "ssn"
+outNames[1] = "first name"
+fieldExpr1 := FieldExpr{td.Fields[1]}
+fieldExpr2 := FieldExpr{td.Fields[2]}
+proj, _ := NewProjectOp([]Expr{&fieldExpr1, &fieldExpr2}, outNames, true, join)
+if proj == nil {
+	t.Fatalf("project was nil")
+}
+
+iter_test2, _ := proj.Iterator(nil)
+for {
+	t, _ := iter_test2()
+	fmt.Println("tup: ")
+	fmt.Println(t)
+	if t == nil {
+		break
+	}
+}
+
+/*aa := EncryptedAvgAggState[string]{}
+expr := FieldExpr{FieldType{Fname: "age", TableQualifier: "t"}}
+aa.Init("avg", &expr, stringAggGetter, *e.PublicKeys["age"])
+agg := NewEncryptedAggregator([]EncryptedAggState{&aa}, proj)
+
+iter, err := agg.Iterator(nil)
+if err != nil {
+	t.Fatalf(err.Error())
+}
+if iter == nil {
+	t.Fatalf("Iterator was nil")
+}
+tup, err := iter()
+if err != nil {
+	t.Fatalf(err.Error())
+}
+if tup == nil {
+	t.Fatalf("Expected non-null tuple")
+}
+
+result, err := e.encryptOrDecryptTuple(tup, false)
+if err != nil {
+	t.Fatalf(err.Error())
+}
+
+sum := result.Fields[0].(IntField).Value
+count := result.Fields[1].(IntField).Value
+
+fmt.Println(sum, count)
+if sum != 1151 || count != 15 {
+	t.Errorf("unexpected sum or count")
+}*/
+/*}
+
+// Encrypted Sum Tests
+func TestEncryptedSumAgg(t *testing.T) {
+	sql := "select sum(age) from t"
+
+	var td = TupleDesc{Fields: []FieldType{
+		{Fname: "id", Ftype: StringType},
+		{Fname: "ssn", Ftype: StringType},
+		{Fname: "first_name", Ftype: StringType},
+		{Fname: "last_name", Ftype: StringType},
+		{Fname: "phone_number", Ftype: StringType},
+		{Fname: "gender", Ftype: StringType},
+		{Fname: "age", Ftype: StringType},
+		{Fname: "diagnosis_code", Ftype: StringType},
+	}}
+	resultFileName := "encryptedresults/1000_encrypted_mock_patient_data.csv"
+
+	// Uncomment Only Below for First Run to Generate Encrypted File
+	// inputFileName := "encryptedresults/1000_mock_patient_data.csv"
+	// encryptedHf, e := CSVToEncryptedDat(td, inputFileName, resultFileName, sql)
+
+	// Uncomment Only Below for Subsequent Runs to Avoid Generating Encrypted File
+	err, e := translateQuery(sql)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	bp := NewBufferPool(10)
+	encryptedHf, err := NewHeapFile(resultFileName, &td, bp)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// ///
+
+	aa := EncryptedSumAggState[string]{}
+	expr := FieldExpr{FieldType{Fname: "age", TableQualifier: "t"}}
+	aa.Init("sum", &expr, stringAggGetter, *e.PublicKeys["age"])
+
+	agg := NewEncryptedAggregator([]EncryptedAggState{&aa}, encryptedHf)
+	iter, err := agg.Iterator(nil)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if iter == nil {
+		t.Fatalf("Iterator was nil")
+	}
+	tup, err := iter()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if tup == nil {
+		t.Fatalf("Expected non-null tuple")
+	}
+	fmt.Println(tup)
+}
+
+// Unencrypted Sum Tests
+func TestSumAgg(t *testing.T) {
+	var td = TupleDesc{Fields: []FieldType{
+		{Fname: "id", Ftype: StringType},
+		{Fname: "ssn", Ftype: StringType},
+		{Fname: "first_name", Ftype: StringType},
+		{Fname: "last_name", Ftype: StringType},
+		{Fname: "phone_number", Ftype: StringType},
+		{Fname: "gender", Ftype: StringType},
+		{Fname: "age", Ftype: IntType},
+		{Fname: "diagnosis_code", Ftype: StringType},
+	}}
+
+	resultFileName := "encryptedresults/1000_unencrypted_mock_patient_data.dat"
+	os.Remove(resultFileName)
+
+	bp := NewBufferPool(10)
+	hf, err := NewHeapFile(resultFileName, &td, bp)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// Uncomment the below on first run to generate dat file
+	inputFileName := "encryptedresults/1000_mock_patient_data.csv"
+	f, err := os.Open(inputFileName)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	err = hf.LoadFromCSV(f, true, ",", false)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	////
+
+	sa := SumAggState[int64]{}
+	expr := FieldExpr{FieldType{Fname: "age", TableQualifier: "t"}}
+	sa.Init("sum", &expr, intAggGetter)
+	agg := NewAggregator([]AggState{&sa}, hf)
+
+	iter, err := agg.Iterator(nil)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if iter == nil {
+		t.Fatalf("Iterator was nil")
+	}
+	tup, err := iter()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if tup == nil {
+		t.Fatalf("Expected non-null tuple")
+	}
+	fmt.Println(tup)
+}*/
